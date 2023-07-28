e777f10963b7383c8784e7ce4639e2dd2932966f
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:18:01 -0700

    Add files via upload

---
+++ FOURBIT_MAGNITUDE_COMPARATOR/FOURBIT_MAGNITUDE_COMPARATOR.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:10:57 07/28/2023 
+// Design Name: 
+// Module Name:    FOURBIT_MAGNITUDE_COMPARATOR 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module FOURBIT_MAGNITUDE_COMPARATOR(A,B,  less, equal,greater);
+    input [3:0] A;
+    input [3:0]B;
+    output less;
+     output equal;
+     output greater;
+     reg less;
+     reg equal;
+     reg greater;
+    always @(A or B)
+     begin
+        if(A > B)  
+		  begin  
+            less = 0;
+            equal = 0;
+            greater = 1;  
+				end
+        else if(A == B)
+		  begin 
+            less = 0;
+            equal = 1;
+            greater = 0;  
+				end
+        else   
+		  begin 
+            less = 1;
+            equal = 0;
+            greater =0;
+        end 
+    end
+endmodule
---
+++ FOURBIT_MAGNITUDE_COMPARATOR/FOURBIT_MAGNITUDE_COMPARATOR_TEST.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   09:11:58 07/28/2023
+// Design Name:   FOURBIT_MAGNITUDE_COMPARATOR
+// Module Name:   D:/20L118/FOURBIT_MAGNITUDE_COMPARATOR/FOURBIT_MAGNITUDE_COMPARATOR_TEST.v
+// Project Name:  FOURBIT_MAGNITUDE_COMPARATOR
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FOURBIT_MAGNITUDE_COMPARATOR
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module FOURBIT_MAGNITUDE_COMPARATOR_TEST;
+
+	// Inputs
+	reg [3:0] A;
+	reg [3:0] B;
+
+	// Outputs
+	wire less;
+	wire equal;
+	wire greater;
+
+	// Instantiate the Unit Under Test (UUT)
+	FOURBIT_MAGNITUDE_COMPARATOR uut (
+		.A(A), 
+		.B(B), 
+		.less(less), 
+		.equal(equal), 
+		.greater(greater)
+	);
+
+	initial begin
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+
+		// Wait 100 ns for global reset to finish
+		  #100;A=4'b0001;B=4'b0100;
+        #100;A=4'b0101;B=4'b0100;
+		  #100;A=4'b0001;B=4'b1100;
+		  #100;A=4'b0011;B=4'b0100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
