f8a2d8021284c570b09463285ef32d259a015bd9
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:16:20 -0700

    Add files via upload

---
+++ FULLADDER/FA.v
@@ -0,0 +1,16 @@
+//20L118 HARIHARAN R R
+module FA(input A,B,C,output sum,carry );
+wire d0,b0,b1;
+halfadder g1(A,B,d0,b0);
+halfadder g2(d0,C,carry,b1);
+or g3(sum,b1,b0);
+endmodule
+
+module halfadder(a,b,s,c);
+input a,b;
+output s,c;
+//assign{c,s} =a+b; 
+assign s=a^b;
+assign c=a&b;
+endmodule
+
---
+++ FULLADDER/FATEST.v
@@ -0,0 +1,42 @@
+//20L118 HARIHARAN R R
+module FATEST;
+
+	// Inputs
+	reg A;
+	reg B;
+	reg C;
+
+	// Outputs
+	wire sum;
+	wire carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	FA uut (
+		.A(A), 
+		.B(B), 
+		.C(C), 
+		.sum(sum), 
+		.carry(carry)
+	);
+
+	initial begin
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+		C = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100 A = 0;B = 0;C = 1;
+		#100 A = 0;B = 1;C = 0;
+		#100 A = 0;B = 1;C = 1;
+		#100 A = 1;B = 0;C = 0;
+		#100 A = 1;B = 0;C = 1;
+		#100 A = 1;B = 1;C = 0;
+		#100 A = 1;B = 1;C = 1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
