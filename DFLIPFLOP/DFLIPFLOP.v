83fe066ccedb061e39bab444b34a90ad72cd4936
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:14:44 -0700

    Add files via upload

---
+++ DFLIPFLOP/DFLIPFLOP.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:12:48 07/26/2023 
+// Design Name: 
+// Module Name:    DFLIPFLOP 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module dflip(d,clk,q,qb);
+input d,clk;
+output q,qb;
+reg q,qb;
+always @(*)
+begin 
+q=d;
+qb=~d;
+end
+endmodule
---
+++ DFLIPFLOP/DFLIPFLOPTEST.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:13:33 07/26/2023
+// Design Name:   dflip
+// Module Name:   D:/20L118/DFLIPFLOP/DFLIPFLOPTEST.v
+// Project Name:  DFLIPFLOP
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: dflip
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module DFLIPFLOPTEST;
+
+	// Inputs
+	reg d;
+	reg clk;
+
+	// Outputs
+	wire q;
+	wire qb;
+
+	// Instantiate the Unit Under Test (UUT)
+	dflip uut (
+		.d(d), 
+		.clk(clk), 
+		.q(q), 
+		.qb(qb)
+	);
+
+	initial begin
+		// Initialize Inputs
+		d = 0;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;d=1'b0;clk=1'b0;
+      #100;d=1'b1;clk=1'b1; 
+		// Add stimulus here
+
+	end
+      
+endmodule
+
