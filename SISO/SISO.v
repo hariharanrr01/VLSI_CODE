cd15a7df3ff21363d3bdf4c0f3f82f6224c6c52b
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:52:39 -0700

    Add files via upload

---
+++ SISO/SISO.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:30:44 07/26/2023 
+// Design Name: 
+// Module Name:    SISO 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module SISO(d,clk,q);
+input d,clk;
+output [3:0]q;
+reg[3:0]q;
+always@(posedge clk)
+begin
+q[3]<=d;
+q[2]<=q[3];
+q[1]<=q[2];
+q[0]<=q[1];
+end
+endmodule
---
+++ SISO/SISOTEST.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:31:31 07/26/2023
+// Design Name:   SISO
+// Module Name:   D:/20L118/SISO/SISOTEST.v
+// Project Name:  SISO
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: SISO
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module SISOTEST;
+
+// Inputs
+reg d;
+reg clk;
+
+// Outputs
+wire [3:0] q;
+
+// Instantiate the Unit Under Test (UUT)
+SISO uut (
+.d(d),
+.clk(clk),
+.q(q)
+);
+always#100 clk=~clk;
+initial begin
+// Initialize Inputs
+d = 0;
+clk = 0;
+
+// Wait 100 ns for global reset to finish
+#100;d=1;
+       
+// Add stimulus here
+
+end
+     
+endmodule
