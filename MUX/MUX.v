43264b6f51afc51ca8bf0832edf579ef7514b3b8
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:48:09 -0700

    Add files via upload

---
+++ MUX/MUX.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:02:47 07/26/2023 
+// Design Name: 
+// Module Name:    MUX 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+//20L118 HARIHARAN R R
+module MUX(i,s,o);
+input[3:0]i;
+input[1:0]s;
+output o;
+assign o=((i[0]&~s[0]&~s[1])|(i[1]&~s[0]&s[1])|(i[2]&s[0]&~s[1])|(i[3]&s[0]&s[1]));
+
+endmodule
---
+++ MUX/MUXTEST.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:03:26 07/26/2023
+// Design Name:   MUX
+// Module Name:   D:/20L118/MUX/MUXTEST.v
+// Project Name:  MUX
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: MUX
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module MUXTEST;
+
+	// Inputs
+	reg [3:0] i;
+	reg [1:0] s;
+
+	// Outputs
+	wire o;
+
+	// Instantiate the Unit Under Test (UUT)
+	MUX uut (
+		.i(i), 
+		.s(s), 
+		.o(o)
+	);
+
+	initial begin
+		// Initialize Inputs
+		i = 0;
+		s = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;i[0]=1'b0;s[0]=1'b0;s[1]=1'b0;
+		#100;i[1]=1'b1;s[0]=1'b1;s[1]=1'b0;
+      #100;i[2]=1'b0;s[0]=1'b0;s[1]=1'b1;
+		#100;i[3]=1'b1;s[0]=1'b1;s[1]=1'b1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
