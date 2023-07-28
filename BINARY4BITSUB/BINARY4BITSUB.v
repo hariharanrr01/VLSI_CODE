500cf5f1db5fc5f46844a999fec39b2a747f1bd7
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:09:13 -0700

    Add files via upload

---
+++ BINARY4BITSUB/BINARY4BITSUB.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:50:42 07/26/2023 
+// Design Name: 
+// Module Name:    BINARY4BITSUB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+//20L118 HARIHARAN R R
+
+module BINARY4BITSUB(a,b,s,cout);
+input [3:0]a;
+input [3:0]b;
+output [3:0]s;
+output cout;
+wire c1,c2,c3;
+fullsub fs0(a[0],~b[0],1'b1,s[0],c1);
+fullsub fs1(a[1],~b[1],c1,s[1],c2);
+fullsub fs2(a[2],~b[2],c2,s[2],c3);
+fullsub fs3(a[3],~b[3],c3,s[3],cout);
+endmodule
+
+//FULL ADDER:
+module fullsub(x,y,z,d,s);
+input x,y,z;
+output d,s;
+wire a,b,c,df,e;
+xor x1(a,x,y);
+not n1(b,x);
+and a1(c,b,y);
+xor x2(d,a,z);
+not n2(df,a);
+and a2(e,df,z);
+or o1(s,e,c);
+endmodule
+
---
+++ BINARY4BITSUB/BINARY4BITSUBTEST.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:53:05 07/26/2023
+// Design Name:   BINARY4BITSUB
+// Module Name:   D:/20L118/BINARY4BITSUB/BINARY4BITSUBTEST.v
+// Project Name:  BINARY4BITSUB
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: BINARY4BITSUB
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module BINARY4BITSUBTEST;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+
+	// Outputs
+	wire [3:0] s;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	BINARY4BITSUB uut (
+		.a(a), 
+		.b(b), 
+		.s(s), 
+		.cout(cout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100 a=4'b1000; b=4'b1000;
+		#100 a=4'b0100; b=4'b0100;
+		#100 a=4'b0010; b=4'b0010;
+		#100 a=4'b0001; b=4'b0001;
+      #100 a=4'b0011; b=4'b0101; 
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
