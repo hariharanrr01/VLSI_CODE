9afdfc0af08fc1d1c080087c7564fbd45ec83597
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 01:44:56 -0700

    Add files via upload

---
+++ MAGNITUDE COMPARATOR/MAG.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:05:37 07/28/2023 
+// Design Name: 
+// Module Name:    MAG 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+//20L118 HARIHARAN R R
+module MAG ( a ,b ,equal ,greater ,lower );
+output equal ;
+output greater ;
+output lower ;
+input a ;
+input b ;
+assign equal = a ~^ b;
+assign lower = (~a) & b;
+assign greater = a & (~b);
+
+endmodule
---
+++ MAGNITUDE COMPARATOR/MAGTEST.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   09:06:45 07/28/2023
+// Design Name:   MAG
+// Module Name:   D:/20L118/MAG/MAGTEST.v
+// Project Name:  MAG
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: MAG
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+//20L118 HARIHARAN R R
+module MAGTEST;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire equal;
+	wire greater;
+	wire lower;
+
+	// Instantiate the Unit Under Test (UUT)
+	MAG uut (
+		.a(a), 
+		.b(b), 
+		.equal(equal), 
+		.greater(greater), 
+		.lower(lower)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=1'b0;b=1'b1;
+      #100;a=1'b1;b=1'b1;  
+		#100;a=1'b0;b=1'b0;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
