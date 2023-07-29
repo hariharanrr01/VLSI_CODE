c8741eb5678e98f7bef30058f5e6bb77ac6fd64b
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-28 23:04:11 -0700

    Add files via upload

---
+++ MEALYMODEL/MEALYMODEL.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:27:16 07/29/2023 
+// Design Name: 
+// Module Name:    MEALYMODEL 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module MEALYMODEL(input x,rst,clk,output reg y);
+reg [1:0]p,n;
+parameter A=2'b00,B=2'b01,C=2'b10,D=2'b11;
+always @(posedge clk,posedge rst)begin
+if(rst)
+begin
+ p=A;
+ y=1'b1;
+ end
+else
+p=n;
+end
+always@(p or x)
+begin
+y=1'b0;
+ case(p)
+ A:
+ if(x) n=B;
+ else n=A;
+ B:
+ if(x) n=B;
+ else  n=C;
+ C: 
+ if(x) n=D;
+ else n=A;
+ D:
+ if(x)begin n=B; y=1'b1; end
+ else  n=C;
+ 
+ endcase
+ end
+
+endmodule
---
+++ MEALYMODEL/MEALYMODELTEST.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   11:29:06 07/29/2023
+// Design Name:   MEALYMODEL
+// Module Name:   D:/20L118/MEALAYMODEL/MEALYMODELTEST.v
+// Project Name:  MEALAYMODEL
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: MEALYMODEL
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module MEALYMODELTEST;
+
+	// Inputs
+	reg x;
+	reg rst;
+	reg clk;
+
+	// Outputs
+	wire y;
+
+	// Instantiate the Unit Under Test (UUT)
+	MEALYMODEL uut (
+		.x(x), 
+		.rst(rst), 
+		.clk(clk), 
+		.y(y)
+	);
+
+	initial begin
+		// Initialize Inputs
+		x = 0;
+		rst = 0;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100 rst=1;x=0;
+		#100 rst=0;x=0;
+		#100 x=1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
