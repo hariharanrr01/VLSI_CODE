a61a8d54506254cb73b1444937112beea303dca2
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-31 00:47:40 -0700

    Add files via upload

---
+++ FSM1/FSM1.v
@@ -0,0 +1,77 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:04:50 07/29/2023 
+// Design Name: 
+// Module Name:    FSM1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module FSM1(seq,clk,rst,dout);
+input clk,rst,seq;
+output reg dout;
+parameter A=2'b00;
+parameter B=2'b01;
+parameter C=2'b10;
+parameter D=2'b11;
+reg[2:0]current,next;
+always @(posedge clk,posedge rst)
+begin
+if(rst)
+current=1'b0;
+else
+current=next;
+end
+always @(current,seq)
+begin
+case(current)
+A:begin
+if(seq)
+next=B;
+else
+next=A;
+end
+B:begin
+if(seq)
+next=C;
+else
+next=B;
+end
+C:begin
+if(seq)
+next=D;
+else
+next=C;
+end
+D:begin
+if(seq)
+next=A;
+ dout=1'b1;
+end
+default:next=A;
+endcase
+end
+always @(current)
+begin 
+ case(current) 
+ A:   dout = 0;
+ B:   dout = 0;
+ C:  dout = 0;
+ D:  dout = 1;
+ default:  dout = 0;
+ endcase
+end 
+endmodule
---
+++ FSM1/FSM1TEST.v
@@ -0,0 +1,74 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:05:30 07/29/2023
+// Design Name:   FSM1
+// Module Name:   D:/20L118/FSM1/FSM1TEST.v
+// Project Name:  FSM1
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FSM1
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module FSM1TEST;
+
+	// Inputs
+	reg seq;
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire dout;
+
+	// Instantiate the Unit Under Test (UUT)
+	FSM1 uut (
+		.seq(seq), 
+		.clk(clk), 
+		.rst(rst), 
+		.dout(dout)
+	);
+
+	initial begin
+   clk = 0;
+   forever #5 clk = ~clk;
+    end 
+		
+		initial begin
+		 seq = 0;
+       rst = 1;
+		 
+		// Wait 100 ns for global reset to finish
+		#30;
+      rst = 0;
+  #60;
+  seq = 1;
+  #30;
+  seq = 0;
+  #100;
+  seq = 1; 
+  #40;
+  seq = 0; 
+  #40;
+  seq = 1; 
+  #40;
+  seq = 0;  
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
