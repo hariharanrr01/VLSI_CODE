9cbc5a829faa0bad4664cc84a732888764885d01
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-31 00:49:48 -0700

    Add files via upload

---
+++ FSM6/FSM6.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:56:49 07/29/2023 
+// Design Name: 
+// Module Name:    FSM6 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module FSM6(seq,clk,rst,dout);
+input clk,rst,seq;
+output reg dout;
+parameter si=2'b00;
+parameter s0=2'b01;
+parameter s1=2'b10;
+reg[2:0]current,next;
+always @(posedge clk,posedge rst)
+begin
+if(rst)
+current=1'b0;
+else
+current=next;
+end
+always @(current,seq)
+begin
+case(current)
+si:begin
+if(seq)
+next=s0;
+else
+next=s1;
+end
+s0:begin
+if(seq)
+next=s1;
+dout=1'b1;
+end
+s1:begin
+if(seq)
+next=s1;
+else
+next=s0;
+end
+default:next=si;
+endcase
+end
+always @(current)
+begin 
+ case(current) 
+ si:   dout = 0;
+ s0:   dout = 1;
+ s1:  dout = 0;
+ default:  dout = 0;
+ endcase
+end 
+endmodule
---
+++ FSM6/FSM6TEST.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:57:39 07/29/2023
+// Design Name:   FSM6
+// Module Name:   D:/20L118/FSM6/FSM6TEST.v
+// Project Name:  FSM6
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FSM6
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+//20L118 HARIHARAN R R
+module FSM6TEST;
+
+	// Inputs
+	reg seq;
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire dout;
+
+	// Instantiate the Unit Under Test (UUT)
+	FSM6 uut (
+		.seq(seq), 
+		.clk(clk), 
+		.rst(rst), 
+		.dout(dout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		seq = 0;
+		clk = 0;
+		rst = 0;
+		end
+		always #100 clk=~clk;
+		initial begin
+		// Wait 100 ns for global reset to finish
+		#10 rst=1'b0;seq=1'b0;
+		#100 rst=1'b0;seq=1'b1;
+		#20 rst=1'b1;seq=1'b0;
+		#20 rst=1'b1;seq=1'b1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
