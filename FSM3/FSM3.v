2f184c341a116889020afdaaa2d4bfa25b8e570e
HARIHARAN R R <128307440+hariharanrr01@users.noreply.github.com>
2023-07-31 00:48:52 -0700

    Add files via upload

---
+++ FSM3/FSM3.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:07:49 07/31/2023 
+// Design Name: 
+// Module Name:    FSM3 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+//20L118 HARIHARAN R R
+module FSM3(seq,clk,rst,dout);
+input clk,rst,seq;
+output reg dout;
+parameter W=2'b00;
+parameter X=2'b01;
+parameter Y=2'b10;
+reg[2:0]current,next;
+always @(posedge clk,posedge rst)
+begin
+if(rst)
+current=1'b0;
+else
+current=next;
+end
+always @(current,seq)
+begin
+case(current)
+W:begin
+if(seq)
+next=X;
+else
+next=Y;
+end
+X:begin
+if(seq)
+next=Y;
+dout=1'b0;
+end
+Y:begin
+if(seq)
+next=W;
+else
+next=X;
+end
+default:next=W;
+endcase
+end
+always @(current)
+begin 
+ case(current) 
+ W:   dout = 0;
+ X:   dout = 1;
+ Y:  dout = 0;
+ default:  dout = 0;
+ endcase
+end 
+endmodule
---
+++ FSM3/FSM3TEST.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   09:08:44 07/31/2023
+// Design Name:   FSM3
+// Module Name:   D:/20L118/FSM3/FSM3TEST.v
+// Project Name:  FSM3
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FSM3
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module FSM3TEST;
+
+	// Inputs
+	reg seq;
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire dout;
+
+	// Instantiate the Unit Under Test (UUT)
+	FSM3 uut (
+		.seq(seq), 
+		.clk(clk), 
+		.rst(rst), 
+		.dout(dout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		seq = 0;
+		clk = 0;
+		rst = 0;
+		end
+
+		// Wait 100 ns for global reset to finish
+		always #100 clk=~clk;
+		initial begin
+		#100 rst=1'b0;seq=2'b00;
+		#100 rst=1'b1;seq=2'b01;
+		#100 rst=1'b0;seq=2'b10;
+		#100 rst=1'b1;seq=2'b11;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
